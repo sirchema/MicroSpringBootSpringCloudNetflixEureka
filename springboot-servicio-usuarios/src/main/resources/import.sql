3264ebc8706b0de902b215e52ad11447b4e578fb
Chema <chema.flo@gmail.com>
2023-02-24 07:21:56 +0100

    springboot-servicio-usuarios

---
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('jose','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',true, 'jose', 'flores','chema@correo.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',true, 'John', 'Doe','jhon.doe@correo.com');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
